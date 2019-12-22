40661b3a15c08121466d625a9294fda4770ce557
Vishvajeet Thakur <41371995+v10s@users.noreply.github.com>
2019-12-22 18:51:10 +0530

    Update main.sql

--- FAST_API/sql/main.sql
+++ FAST_API/sql/main.sql
@@ -1,7 +1,11 @@
-SELECT * FROM pg_available_extensions;
-
 CREATE EXTENSION IF NOT EXISTS "cube";
 
 CREATE EXTENSION IF NOT EXISTS "earthdistance";
+
+
+/* must set successfully install the postgis in your postgres server to run */
+CREATE EXTENSION IF NOT EXISTS "postgis";
+
+
 
 CREATE TABLE pin ( key CHAR(10) PRIMARY KEY , place_name VARCHAR(50) NOT NULL,admin_name1 VARCHAR(50) NOT NULL, latitude double precision ,longitude double precision ,accuracy double precision);
